7ea87a206c34d2582b4ca7bf9cfb1cfd36805a35
DESKTOP-DTHQ2HP\john8 <john82599@gmail.com>
2024-11-05 17:31:06 -0500

    change api client to use the VITE_API_BASE_URL or empty

--- frontend/src/js/ApiClient.jsx
+++ frontend/src/js/ApiClient.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 // Use a default value if the environment variable is not set
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
-
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
 const ApiClient = {
     makeRequest: async (method, route, data = {}, responseType = 'json') => {
         try {
